d9486a9f82e431170959a13ec4e6916a93ac26a1
Paul <paulseidler1@gmail.com>
2017-11-23 19:24:38 +0100

    new ACL + start Role Based Access Control

---
+++ README.md.orig
@@ -0,0 +1,9 @@
+# terra0 🌲🌳🌿
+terra0 is a collection of decentralized programs / smartcontracts for environmental management and tokenizing of natural resources. The inital contracts where written as a prototype. Everything in this repo is therefore highly experimental software and should not be used in production.
+
+For more infos see terra0.org
+or talk to us in our slack:
+http://slack.terra0.org/
+
+
+
--- terra0/contracts/acl.sol
+++ terra0/contracts/acl.sol
@@ -1,7 +1,13 @@
 pragma solidity 0.4.18;
 
-contract acl{
+//This is the first version of a simple ACL / Permission Management System
+//It might differentiate from other Permission Management Systems and therefore be more restrictive in the following points:
+// Every User can just have one Role (similar to a state machine)
+// No new Roles "Positions" can be generated
+// Therefore all possible Roles must be defined at the beginning
 
+
+contract acl{
 
     enum Role {
         ANON,
