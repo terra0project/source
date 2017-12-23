02cc1189127aaf1fcbfab3e2e7d86bbd6b8cbe4d
Paul <paulseidler1@gmail.com>
2017-12-23 20:47:29 +0100

    Add Transfer

--- terra0/contracts/treeregister.sol
+++ terra0/contracts/treeregister.sol
@@ -1,24 +1,27 @@
 pragma solidity ^0.4.6;
 
-///Not Tested and no ACL Implementation
-/// Might merged together with an ERC721 Implementation 
 
 contract treeregister {
 
   struct EntityStruct {
-    uint entityData;
+    bytes BCHTransactionid;
     bool isEntity;
   }
 
- mapping(address => mapping(bytes32 => EntityStruct)) public entityStructs;
- mapping(address => uint)NumberofIds;
- address[] public OwnerList;
- bytes32[] public IDList;
+
+  mapping(address => mapping(bytes32 => EntityStruct)) internal entityStructs;
+  mapping(address => uint)internal NumberofAssets;
+  mapping(address => bool) internal KnownAdresses;
+  address[] internal OwnerList;
+  bytes32[] internal IDList;
 
 
-function isEntity(address entityAddress,bytes32 uniqueID) public
-constant returns(bool isIndeed) {
+  function isEntity(address entityAddress,bytes32 uniqueID) public constant returns(bool isIndeed) {
       return entityStructs[entityAddress][uniqueID].isEntity;
+  }
+
+  function isAdressknown(address entityAddress) public constant returns(bool isIndeed) {
+      return KnownAdresses[entityAddress];
   }
 
   function getAllOwner() public constant returns(uint entityCount) {
@@ -28,27 +31,44 @@ constant returns(bool isIndeed) {
   function getAllTrees() public constant returns(uint entityCount) {
     return IDList.length;
   }
-    function getTreesof(address entityAddress) public constant
-returns(uint entityCount) {
-    return NumberofIds[entityAddress] ;
+
+  function getTreesof(address entityAddress) public constant returns(uint entityCount) {
+    return NumberofAssets[entityAddress] ;
   }
 
-  function newEntity(address entityAddress, uint entityData) public
-returns(uint rowNumber, bytes32 _UniqueID) {
+  function newEntity(address entityAddress, bytes entityData) public returns(uint rowNumber, bytes32 _UniqueID) {
     bytes32 UniqueID = keccak256(entityData);
     if(isEntity(entityAddress,UniqueID)) revert();
-    entityStructs[entityAddress][UniqueID].entityData = entityData;
+    entityStructs[entityAddress][UniqueID].BCHTransactionid = entityData;
     entityStructs[entityAddress][UniqueID].isEntity = true;
+    NumberofAssets[entityAddress]= NumberofAssets[entityAddress] +1;
     IDList.push(UniqueID);
-    NumberofIds[entityAddress]+1;
-    return (OwnerList.push(entityAddress) - 1,UniqueID);
+    if  (isAdressknown(entityAddress)){
+    return (0,UniqueID);
+    }else{
+    KnownAdresses[entityAddress] = true;
+    return (OwnerList.push(entityAddress) - 1,UniqueID);  //somethingdontwork here
+    }
+    }
+
+  function updateData(address entityAddress,bytes32 uniqueID,bytes entityData) public returns(bool success) {
+    if(!isEntity(entityAddress,uniqueID)) revert();
+    entityStructs[entityAddress][uniqueID].BCHTransactionid = entityData;
+    return true;
   }
 
-  function updateEntity(address entityAddress,bytes32 uniqueID,uint
-entityData) public returns(bool success) {
-    if(!isEntity(entityAddress,uniqueID)) revert();
-    entityStructs[entityAddress][uniqueID].entityData    = entityData;
+  function transferownership(address Owner, address NewOwner,bytes32 uniqueID)public returns(bool success){
+    if(isEntity(Owner,uniqueID)){
+    entityStructs[NewOwner][uniqueID].BCHTransactionid = entityStructs[Owner][uniqueID].BCHTransactionid;
+    entityStructs[NewOwner][uniqueID].isEntity = true;
+    entityStructs[Owner][uniqueID].isEntity = false;
+    NumberofAssets[Owner]= NumberofAssets[Owner] -1;
+    NumberofAssets[NewOwner]= NumberofAssets[NewOwner] +1; ///needs to add new ownwers
     return true;
+    }
+    else{
+    return false;
+    }
   }
 
 }
