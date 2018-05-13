a2d35fb430a1b8f363b4e53aaaa8a3b6191810d7
Max F Hampshire <mfahampshire@Maxs-MBP.fritz.box>
2018-05-13 23:38:02 +0200

    .

--- ERC721/contracts/buyable.sol
+++ ERC721/contracts/buyable.sol
@@ -1,10 +1,6 @@
 pragma solidity ^0.4.23;
 import './update.sol';
 
-// All functions will only work if user verified contract as operator before:
-// this can be done:
-// function setApprovalForAll("Adress_of_contract", "True")
-
 contract buyable is update {
 
     address BLOOMING_POOL_ADDRESS;
@@ -22,57 +18,57 @@ contract buyable is update {
 
     function set_price_and_sell(uint256 UniqueID,uint256 Price) external payable returns (address){
     TokenIdtoprice[UniqueID] = Price;
-    this.approve(address(this), UniqueID);
+    approve(address(this), UniqueID);
     emit Set_price_and_sell(UniqueID, Price);
     }
 
     function stop_sell(uint256 UniqueID) external payable{
-    require(TokenIdtoadress[UniqueID] == msg.sender );
-    _clearTokenApproval(UniqueID);
+    require(tokenOwner[UniqueID] == msg.sender );
+    clearApproval(tokenOwner[UniqueID],UniqueID);
     emit Stop_sell(UniqueID);
     }
 
-	function buy(uint256 UniqueID)external payable{
+function buy(uint256 UniqueID)external payable{
 		    address _to =  msg.sender;
 		    uint _total = msg.value;
-			if (_total !=TokenIdtoprice[UniqueID].toWei()){
+			if (_total !=TokenIdtoprice[UniqueID]){
 				revert();
 			} else {
 				uint _blooming = (_total / 20);
 			    uint _infrastructure = (_blooming / 20);
 				uint _amount_for_seller = msg.value.sub(_blooming + _infrastructure);
-				address _seller = TokenIdtoadress[UniqueID];
+				address _seller = tokenOwner[UniqueID];
 				_seller.transfer(_amount_for_seller);
-			    this.transferFrom(TokenIdtoadress[UniqueID], _to, UniqueID);
+			    transferFrom(tokenOwner[UniqueID], _to, UniqueID);
 			    BLOOMING_POOL_ADDRESS.transfer(_blooming);
 			    INFRASTRUCTURE_POOL_ADDRESS.transfer(_infrastructure);
 			}
 	    }
 
     function get_token_data(uint256 _tokenId) external view returns(string _health,string _height, string _blooming,uint256 _price, bool _buyable ){
-	    _health =  TokenId[_tokenId].health;
-	    _height = TokenId[_tokenId].height;
-	    _blooming = TokenId[_tokenId].blooming;
-	    _price = TokenIdtoprice[_tokenId];
-	    if (TokenIdToApprovedAddress[_tokenId] != address(0)){
-	        _buyable = true;
-	    }
-	}
+    _health =  TokenId[_tokenId].health;
+    _height = TokenId[_tokenId].height;
+    _blooming = TokenId[_tokenId].blooming;
+    _price = TokenIdtoprice[_tokenId];
+    if (tokenOwner[_tokenId] != address(0)){
+        _buyable = true;
+    }
+}
 
     function get_token_data_buyable(uint256 _tokenId) external view returns(bool _buyable ){
-	    if (TokenIdToApprovedAddress[_tokenId] != address(0)){
-	        _buyable = true;
-	    }
+    if (tokenOwner[_tokenId] != address(0)){
+        _buyable = true;
+    }
     }
 
     function get_all_sellable_token()external view returns(bytes1[101] list_of_available){
-	    for(uint i;i<101;i++) {
-	        if (TokenIdToApprovedAddress[i-1] != address(0)){
-	        	list_of_available[i] = 0x01;
-	        }else{
-	        	list_of_available[i] = 0x00;
-	        }
-	    }
-	}
+    for(uint i;i<101;i++) {
+          if (tokenOwner[i-1] != address(0)){
+            list_of_available[i] = 0x01;
+          }else{
+            list_of_available[i] = 0x00;
+          }
+        }
+    }
 
 }
