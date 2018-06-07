fca5a166b683528fc7daa202384b32997465950d
Max F Hampshire <mfahampshire@Maxs-MBP.fritz.box>
2018-06-07 12:31:06 +0200

    fixed type error in truffle.js & truffle-config.js

--- ERC721/truffle-config.js
+++ ERC721/truffle-config.js
@@ -1,5 +1,6 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
 const dotenv = require('dotenv')
+require('dotenv').config()
 
 var mnemonic = process.env.MNEMONIC;
 var infura = process.env.INFURA;
@@ -10,12 +11,12 @@ module.exports = {
     		host: "localhost",
     		port: 8545,
     		network_id: "*" // Match any network id
-		}
-	},
+		},
 		ropsten: {
 			provider: function() {
 				return new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infura}`)
 			},
 			network_id: 3
 		}
+	}
 };
