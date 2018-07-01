4ec0c41fb4ed91d31207f938bbbba14b003b4a08
Paul <paulseidler1@gmail.com>
2018-07-01 18:49:25 +0200

    rewrote bloomingPool

--- ERC721/contracts/bloomingPool.sol
+++ ERC721/contracts/bloomingPool.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.23;
-import './update.sol';
-import './SafeMath.sol';
+import "./update.sol";
+import "./SafeMath.sol";
 
 /// @dev altered version of Open Zepplin's 'SplitPayment' contract
 
@@ -12,64 +12,71 @@ contract bloomingPool is update {
     uint256 public totalReleased = 0;
 
     mapping(address => uint256) public shares;
-    mapping(address => uint256) public released;
-    address[] public payees;
+    //mapping(address => uint256) public released;
 
-	constructor() public {}
+    constructor() public {}
 
-	function() public payable { } // fallback function for payment acceptance
+    function() public payable { } // fallback function for payment acceptance
 
-    function addPayee(address _payee, uint256 _shares) internal {
-		if (_payee != address(0)) {
-			require(_shares > 0);
-	        require(shares[_payee] == 0);
 
-	        payees.push(_payee);
-	        shares[_payee] = _shares;
-	        totalShares = totalShares.add(_shares);
-		}
+    function calculate_total_shares(uint256 _shares,uint256 unique_id )internal{
+        shares[tokenOwner[unique_id]] = shares[tokenOwner[unique_id]].add(_shares);
+        totalShares = totalShares.add(_shares);
+
     }
 
-	function oracle_call() external check(1){
-		check_blooming();
-		for (uint i=0;i<payees.length;i++){
-			address to = payees[i];
-			payout(to);
-		}
-		reset_shares();
-		}
+    function oracle_call() external check(1){
+        check_blooming();
 
-	function check_blooming() internal {
-		for(uint i;i<101;i++) {
-			if (compareStrings(TokenId[i].blooming, "1") == true) {
-				addPayee(tokenOwner[i],1);
-			}
-		}
-	}
+    }
 
-	function payout(address to) internal returns(bool){
-		address payee = to;
-		require(shares[payee] > 0);
+    function get_shares() public returns(uint256 individual_shares){
+        return shares[msg.sender];
 
-        uint256 totalReceived = address(this).balance.add(totalReleased);
-        uint256 payment = totalReceived.mul(shares[payee]).div(totalShares).sub(released[payee]);
+    }
+
+    function check_blooming() internal {
+        for(uint i;i<101;i++) {
+            if (compareStrings(TokenId[i].blooming, "1") == true) {
+                calculate_total_shares(1,i);
+            }
+        }
+    }
+
+    function reset_individual_shares(address payee)internal {
+        shares[payee] = 0;
+    }
+
+    function substract_individual_shares(uint256 shares)internal {
+        totalShares = totalShares - shares;
+    }
+
+
+    function claim()public{
+        payout(msg.sender);
+    }
+
+    function payout(address to) internal returns(bool){
+        address payee = to;
+        require(shares[payee] > 0);
+
+        uint256 volume = address(this).balance;   ///totalReleased need to go :(
+        uint256 payment = volume.mul(shares[payee]).div(totalShares);
+        /* uint256 totalReceived = address(this).balance.add(totalReleased);
+        uint256 payment = totalReceived.mul(shares[payee]).div(totalShares).sub(released[payee]); */
 
         require(payment != 0);
         require(address(this).balance >= payment);
 
-        released[payee] = released[payee].add(payment);
+        //released[payee] = released[payee].add(payment);
         totalReleased = totalReleased.add(payment);
-
         payee.transfer(payment);
-	}
+        substract_individual_shares(shares[payee]);
+        reset_individual_shares(payee);
+    }
 
-	function reset_shares() internal {
-		if (totalShares < 0){
-			totalShares = 0;
-		}
-	}
 
-	function compareStrings (string a, string b) internal returns(bool){
+    function compareStrings (string a, string b) internal returns(bool){
         return keccak256(a) == keccak256(b);
     }
 
